4511f642d0a612f1fd7bcc4154e9e7a8e159daef
CAI <3605744@ppti-14-508-08.ufr-info-p6.jussieu.fr>
2017-04-20 14:01:43 +0200

    question3

--- exercice_un.c
+++ exercice_un.c
@@ -1,4 +1,4 @@
-/* BLOTTIERE--MAYO Robin 3200248
+/* BLOTTIERE--MAYO Robin 3200248 
  * CAI Qiaoshan 3605744
  */
 
@@ -21,157 +21,166 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /* ---------- I. Initialisation du Systeme ----------- */
 void initialization(void) {
-	int id[NB_PAIR], succ[NB_PAIR], resp[NB_PAIR], rang_succ;
-	int searching_pair, deconnect_pair,deconnect_suc, deconnect_pre;
-	int r, s, b;
-	int i, j, tmp;
-	
-	for(i = 0; i < NB_PAIR; i++) {
-		id[i] = SIZE_MAX;
-		succ[i] = SIZE_MAX;
-		resp[i] = SIZE_MAX;
-	}
-	/* Initialize id. */
-	for(i = 0; i < NB_PAIR; i++) {
-		do {
-			r = rand() % SIZE_MAX;
-			if(id[i] == SIZE_MAX) {
-				/* Verify unity. */
-				b = 1;
-				for(j = 0; j < NB_PAIR+i; j++) {
-					if(id[j%NB_PAIR] == r) {
-						b = 0;
-						break;
-					}
-				}
-				if(b == 1)
-					id[i] = r;
-			}
-		} while(id[i] == SIZE_MAX);
-	}
-	/* Sort tab : */
-	for(i = 0; i < NB_PAIR; i++) {
-		tmp = id[i];
-		for(j = i+1; j < NB_PAIR; j++) {
-			if(id[i] > id[j]){
-				tmp = id[j];
-				id[j]= id[i];
-				id[i] = tmp;
-			}
-		}
-	}
-	
-	
-	/* Initialization of succ : */
-	for(i = 0; i < NB_PAIR; i++) {
-		succ[i] = id[(i+1)%NB_PAIR];
-	}
-	
-	/* Initialization of resp : */
-	for(i = 1; i < NB_PAIR+1; i++) {
-		resp[i%NB_PAIR] = id[(i-1)%NB_PAIR] + 1;
-	}
-	
-	for(i = 0; i < NB_PAIR; i++) {
-		printf("id[%d] : %d,\t succ : %d\tresp : %d\n", i+1, id[i], succ[i], resp[i]);
-	}
-	
-	/* Send to nodes : */
-	for(i=1; i<=NB_PAIR; i++) {
-		rang_succ = (i%NB_PAIR) + 1;
-		MPI_Send(&id[i-1], 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
-		MPI_Send(&succ[i-1], 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
-		MPI_Send(&resp[i-1], 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
-		MPI_Send(&rang_succ, 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
-	}
-	
-	/* ----------- II. Recherche d une donnee : ----------- */
-	var_to_find = rand() % SIZE_MAX;
-	printf("Key to find = %d\n", var_to_find);
-	searching_pair = (rand() % NB_PAIR) + 1;
-	printf("Searching pair : %d\n", searching_pair);
-	sleep(1);
-	MPI_Send(&var_to_find, 1, MPI_INT, searching_pair, searching_pair, MPI_COMM_WORLD);
-	
-	/* ----------- III. Gestion de la Dynamicite du Systeme : ----------- */
-	deconnect_pair = (rand() % NB_PAIR) + 1;
-	deconnect_suc = (deconnect_pair+1)% NB_PAIR;
-	deconnect_pre = (deconnect_pair-1)% NB_PAIR;
-	i = succ[deconnect_pair-1];
-	j = resp[deconnect_pair-1];
-	
-	sleep(4);
-	MPI_Send(&i, 1, MPI_INT, deconnect_suc, TAGRESP, MPI_COMM_WORLD);
-	MPI_Send(&j, 1, MPI_INT, deconnect_pre, TAGSUCC, MPI_COMM_WORLD);
-	printf("Peer %d is deconnected, succ is %d, resp is %d \n", deconnect_pair, i, j);
+  int id[NB_PAIR], succ[NB_PAIR], resp[NB_PAIR], rang_succ;
+  int searching_pair, deconnect_pair,deconnect_suc, deconnect_pre;
+  int r, s, b;
+  int i, j, tmp;
+
+  for(i = 0; i < NB_PAIR; i++) {
+    id[i] = SIZE_MAX;
+    succ[i] = SIZE_MAX;
+    resp[i] = SIZE_MAX;
+  }
+  /* Initialize id. */
+  for(i = 0; i < NB_PAIR; i++) {
+    do {
+      r = rand() % SIZE_MAX;
+      if(id[i] == SIZE_MAX) {
+        /* Verify unity. */
+        b = 1;
+        for(j = 0; j < NB_PAIR+i; j++) {
+          if(id[j%NB_PAIR] == r) {
+            b = 0;
+            break;
+          }
+        }
+        if(b == 1)
+          id[i] = r;
+      }
+    } while(id[i] == SIZE_MAX);
+  }
+  /* Sort tab : */
+  for(i = 0; i < NB_PAIR; i++) {
+    tmp = id[i];
+    for(j = i+1; j < NB_PAIR; j++) {   
+      if(id[i] > id[j]){  
+        tmp = id[j];
+        id[j]= id[i];
+        id[i] = tmp;
+      }
+    }
+  }
+  
+
+  /* Initialization of succ : */
+  for(i = 0; i < NB_PAIR; i++) {
+    succ[i] = id[(i+1)%NB_PAIR];
+  }
+
+  /* Initialization of resp : */
+  for(i = 1; i < NB_PAIR+1; i++) {
+    resp[i%NB_PAIR] = id[(i-1)%NB_PAIR] + 1;
+  }
+
+  for(i = 0; i < NB_PAIR; i++) {
+    printf("id[%d] : %d,\t succ : %d\tresp : %d\n", i, id[i], succ[i], resp[i]);
+  }
+
+  /* Send to nodes : */
+  for(i=1; i<=NB_PAIR; i++) {
+    rang_succ = (i%NB_PAIR) + 1;
+    MPI_Send(&id[i], 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
+    MPI_Send(&succ[i], 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
+    MPI_Send(&resp[i], 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
+    MPI_Send(&rang_succ, 1, MPI_INT, i, TAGINIT, MPI_COMM_WORLD);
+  }
+
+  /* ----------- II. Recherche d une donnee : ----------- */
+  var_to_find = rand() % SIZE_MAX;
+  printf("Key to find = %d\n", var_to_find);
+  searching_pair = (rand() % NB_PAIR) + 1;
+  printf("Searching pair : %d\n", searching_pair);
+  MPI_Send(&var_to_find, 1, MPI_INT, searching_pair, TAGINIT, MPI_COMM_WORLD);
+
+/* ----------- III. Gestion de la Dynamicite du Systeme : ----------- */
+  
+  deconnect_pair = (rand() % NB_PAIR) + 1;    //1--NB_PAIR+1
+  if(deconnect_pair == NB_PAIR+1){
+		deconnect_suc = 1; 
+		deconnect_pre = deconnect_pair-1;     
+  }else{
+ 		deconnect_suc = deconnect_pair+1;  
+		deconnect_pre = deconnect_pair-1;  
+  }
+  if(deconnect_pair == 1){
+		deconnect_pre = NB_PAIR+1;
+		deconnect_suc = deconnect_pair+1;  
+  }else{
+ 		deconnect_pre = deconnect_pair-1; 
+		deconnect_suc = deconnect_pair+1;    
+  }
+    
+  
+  i = succ[deconnect_pair-1];
+  j = resp[deconnect_pair-1];
+  
+  MPI_Send(&i, 1, MPI_INT, deconnect_suc, TAGRESP, MPI_COMM_WORLD);
+  MPI_Send(&j, 1, MPI_INT, deconnect_pre, TAGSUCC, MPI_COMM_WORLD);
+  printf("Pair %d deconnect, succ est %d, resp est %d \n", deconnect_pair,i,j);
+
 }
 
+
+
 void pair(int rang) {
-	int rang_succ, id, succ, resp;
-	int key_to_find, deconnect, i;
-	int searching_pair = 0;
-	MPI_Status status;
-	
-	/* I. Initialisation. */
-	MPI_Recv(&id, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
-	MPI_Recv(&succ, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
-	MPI_Recv(&resp, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
-	MPI_Recv(&rang_succ, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
-	
-	/* II. Recherche d une donnee : */
-	MPI_Recv(&key_to_find, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-	searching_pair = status.MPI_TAG;
-	
-	if (key_to_find == -1) {
-		puts("Key was found, step 3 is skipped by this peer.");
+  int rang_succ, id, succ, resp;
+  int key_to_find, deconnect;
+  MPI_Status status;
+
+  /* Initialisation. */
+  MPI_Recv(&id, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
+  MPI_Recv(&succ, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
+  MPI_Recv(&resp, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
+  MPI_Recv(&rang_succ, 1, MPI_INT, 0, TAGINIT, MPI_COMM_WORLD, NULL);
+  pthread_mutex_lock(&mutex);
+  pthread_mutex_unlock(&mutex);
+
+  MPI_Recv(&key_to_find, 1, MPI_INT, MPI_ANY_SOURCE, TAGINIT, MPI_COMM_WORLD, &status);
+
+  if(key_to_find <= id && key_to_find >= resp){
+   		printf("Pair %d is responsible of key %d\n.", rang, key_to_find);
+ 	}
+  else {
+	if (rang == NB_PAIR && key_to_find >= id) printf("Pair %d is responsible of key %d.\n", 1, key_to_find);
+	else{
+		if(status.MPI_SOURCE != 0)
+			printf("Pair %d search key %d.\n", status.MPI_SOURCE, key_to_find);
 		MPI_Send(&key_to_find, 1, MPI_INT, rang_succ, TAGINIT, MPI_COMM_WORLD);
 	}
-	else {
-		if(key_to_find <= id && key_to_find >= resp) {
-			printf("Pair %d is responsible of key %d.\n", rang, key_to_find);
-			key_to_find = -1;
-			MPI_Send(&key_to_find, 1, MPI_INT, rang_succ, TAGINIT, MPI_COMM_WORLD);
-		}
-		else {
-			if(status.MPI_SOURCE != 0)
-				printf("Pair %d search key %d.\n", status.MPI_SOURCE, key_to_find);
-			MPI_Send(&key_to_find, 1, MPI_INT, rang_succ, searching_pair, MPI_COMM_WORLD);
-		}
-	}
+  }
+
+  MPI_Recv(&deconnect, 1, MPI_INT, MPI_ANY_SOURCE, TAGSUCC, MPI_COMM_WORLD, &status);
+  resp = deconnect;
+  printf("resp de Pair %d is %d\n.", rang, resp);
 	
-	printf("Peer %d is waiting.\n", rang);
-	MPI_Recv(&id, 1, MPI_INT, MPI_ANY_SOURCE, TAGSUCC, MPI_COMM_WORLD, &status);
-	resp = deconnect;
-	printf("resp de Pair %d is %d\n.", rang, resp);
-	
-	MPI_Recv(&deconnect, 1, MPI_INT, MPI_ANY_SOURCE, TAGRESP, MPI_COMM_WORLD, &status);
-	succ = deconnect;
-	printf("succ de Pair %d is %d\n.", rang, succ);
+  MPI_Recv(&deconnect, 1, MPI_INT, MPI_ANY_SOURCE, TAGRESP, MPI_COMM_WORLD, &status);
+  succ = deconnect;
+  printf("succ de Pair %d is %d\n.", rang, succ);
 }
+
 
 
 int main (int argc, char* argv[]) {
-	int nb_proc, rang;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &nb_proc);
-	
-	srand(time(NULL));
-	
-	if (nb_proc != NB_PAIR+1) {
-		printf("Nombre de processus incorrect !\n");
-		MPI_Finalize();
-		exit(2);
-	}
-	
-	MPI_Comm_rank(MPI_COMM_WORLD, &rang);
-	
-	if (rang == 0) {
-		initialization();
-	} else {
-		pair(rang);
-	}
-	
-	MPI_Finalize();
-	return 0;
-}
+  int nb_proc, rang;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nb_proc);
 
+  srand(time(NULL));
+
+  if (nb_proc != NB_PAIR+1) {
+    printf("Nombre de processus incorrect !\n");
+    MPI_Finalize();
+    exit(2);
+  }
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rang);
+
+  if (rang == 0) {
+    initialization();
+  } else {
+    pair(rang);
+  }
+
+  MPI_Finalize();
+  return 0;
+}
